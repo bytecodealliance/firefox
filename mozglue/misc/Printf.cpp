5a51f0e21f211546d72767b41c8db2252051e635
Sandor Molnar <smolnar@mozilla.com>
2026-02-06 15:39:09 +0200

    Revert "Bug 2013230 - Use strnlen in mozilla::PrintfTarget::cvt_s. r=glandium" for causing lint failures @ Printf.cpp
    This reverts commit 6d38f96ad4febed04a8eaaf26a93d4edc88ba61c.

--- mozglue/misc/Printf.cpp
+++ mozglue/misc/Printf.cpp
@@ -386,7 +386,7 @@ bool mozilla::PrintfTarget::cvt_s(const char* s, int width, int prec,
   }
 
   // Limit string length by precision value
-  int slen = int(strnlen(s, prec));
+  int slen = int(strlen(s));
   if (0 < prec && prec < slen) {
     slen = prec;
   }
